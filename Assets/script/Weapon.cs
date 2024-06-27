5d1da741a1cb319e226f2b08da1430524606e09e
ksohui <os359@naver.com>
2024-06-27 15:17:24 +0900

    vr fruit

---
+++ Assets/script/Weapon.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject effect;
+
+    AudioSource audio;
+
+    static int score;
+    public Text textScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+                                                                                                        //out 은 hit 의 결과값이 돌아온다 // 5미터까지 /생략하면 무한댇
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 5))
+        {
+            Destroy(hit.transform.gameObject);
+
+            Instantiate(effect, hit.point, Quaternion.LookRotation(hit.normal));
+            audio.Play();
+
+            score += 10;
+            textScore.text = "Score : " + score;
+        }
+    }
+}
---
+++ Assets/script/spawn.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    public GameObject[ ]  prefab;
+    public Transform[ ] pos;
+
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {   //반복해서 불러
+       InvokeRepeating("CreateFruit", 2, 2);
+        audio = GetComponent<AudioSource>();
+    }
+
+    void CreateFruit()
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            int iFruit = Random.Range(0, prefab.Length);
+            int iPos = Random.Range(0, pos.Length);
+            GameObject o = Instantiate(prefab[iFruit], pos[iPos].position, Quaternion.identity);
+
+            Destroy(o, 5f);
+
+            Rigidbody rb = o.GetComponent<Rigidbody>();
+            rb.AddForce(Vector3.up * Random.Range(10f, 15f), ForceMode.VelocityChange);
+        }
+
+        audio.Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
